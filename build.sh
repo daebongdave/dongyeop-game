3bf44bef50711a55f439d071faf6df686947fae7
daebongdave <153154517+daebongdave@users.noreply.github.com>
2024-11-15 21:30:09 +0900

    Update build script with Python installation

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# Install Python
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install 3.11.0
+pyenv global 3.11.0
+
+# Install dependencies
+python -m pip install --upgrade pip
 pip install pygbag
+
+# Build
 pygbag dongyeop_game/
